3e40964aa1d5fc9e108b314afc199ad0e5ae614a
Mohanish <127211714+TEIMohanish@users.noreply.github.com>
2024-08-22 17:14:56 +0530

    Update api.py

--- bot/services/tplay/api.py
+++ bot/services/tplay/api.py
@@ -22,7 +22,7 @@ def within_12_hours(timestamp):
 
 class TPLAY_API():
     API_ALL_CHANNELS = "https://kong-tatasky.videoready.tv/content-detail/pub/api/v1/channels?limit=1000"
-    FETCHER = "https://fox.toxic-gang.xyz/tata/channels"
+    FETCHER = "https://babel-in.xyz/tata/channels"
     HMAC = "https://fox.toxic-gang.xyz/tata/hmac{}".format(random.randint(10,99))
     HMAC_v2 = "https://fox.toxic-gang.xyz/tata/sliv/{}".format(random.randint(10,99))
     def __init__(self, channel_slug):
